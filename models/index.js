8b04c29be1afa594d7c95a7aa552c2e10333a176
Shayan Sheikh <najam.shayan@gmail.com>
2017-11-10 17:01:10 -0500

    Set up getter

--- models/index.js
+++ models/index.js
@@ -3,25 +3,42 @@ var db = new Sequelize('postgres://localhost:5432/wikistack');
 
 var Page = db.define('page', {
   title: {
-      type: Sequelize.STRING
+      type: Sequelize.STRING,
+      allowNull: false
   },
   urlTitle: {
-      type: Sequelize.STRING
+      type: Sequelize.STRING,
+      allowNull: false
   },
   content: {
-      type: Sequelize.TEXT
+      type: Sequelize.TEXT,
+      allowNull: false
   },
   status: {
       type: Sequelize.ENUM('open', 'closed')
+  },
+  date: {
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.NOW
+  },
+  getterMethods: {
+    route() {
+      return '/wiki/' + this.urlTitle;
+    }
   }
 });
 
 var User = db.define('user', {
   name: {
-      type: Sequelize.STRING
+      type: Sequelize.STRING,
+      allowNull: false
   },
   email: {
-      type: Sequelize.STRING
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+        isEmail: true
+    }
   }
 });
 
