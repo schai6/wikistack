4d43b4ebf746cba35edf8f3b5af1d7a824c4f2f8
Shayan Sheikh <najam.shayan@gmail.com>
2017-11-10 17:05:53 -0500

    Started routes

---
+++ routes/index.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+var wikiRouter = require('./wiki');
+var userRouter = require('./user');
+
+module.exports = router;
+router.use('/wiki', wikiRouter);
